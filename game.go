b04b8aff206d44d49a9e5138638326bea81105ca
Jack Lay <42285595+jackhlay@users.noreply.github.com>
2024-02-19 16:18:47 -0700

    Gamestate, command building
    Added gamestate struct to track state, Creates board. Parsing soon

--- game.go
+++ game.go
@@ -2,8 +2,23 @@ package main
 
 import "fmt"
 
+type gameState struct {
+	turn   string
+	board  [8][8]Space
+	pieces [16]Piece
+}
+
 func startIt() {
 	// Create a new board
-	board := Board{}
-	fmt.Print(board)
+	board := Board{
+		Spaces: [8][8]Space{},
+		Pieces: [16]Piece{},
+	}
+	state := gameState{
+		turn:   "white",
+		board:  board.Spaces,
+		pieces: board.Pieces,
+	}
+
+	fmt.Print(state)
 }
